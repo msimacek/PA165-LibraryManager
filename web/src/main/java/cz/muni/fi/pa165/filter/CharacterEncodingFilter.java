e5f4d523c3442b9fb2e205535d5616aa187dd5fe
Juraj Tomko <duri.tomko@gmail.com>
2016-01-23 22:24:32 +0100

    [Add] added CharacterEncodingFilter

--- web/src/main/java/cz/muni/fi/pa165/filter/CharacterEncodingFilter.java
+++ web/src/main/java/cz/muni/fi/pa165/filter/CharacterEncodingFilter.java
@@ -1,5 +1,8 @@
-/*
+
 package cz.muni.fi.pa165.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -7,21 +10,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/*"})
+@WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {}
+    final static Logger log = LoggerFactory.getLogger(CharacterEncodingFilter.class);
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
+    public void doFilter(ServletRequest r, ServletResponse s, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) r;
+        HttpServletResponse response = (HttpServletResponse) s;
         request.setCharacterEncoding("utf-8");
+        log.trace("doFilter {}", request.getRequestURL());
         filterChain.doFilter(request, response);
     }
 
-    @Override
-    public void destroy() {}
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.debug("filter initialized ...");
+    }
+
+    public void destroy() {
+    }
 }
-*/
