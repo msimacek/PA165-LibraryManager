9f0e02c1602f7aae444753393a7e6b7b8c378e2c
Jakub Peschel <jakubpeschel@gmail.com>
2016-01-26 12:34:46 +0100

    Added javadoc about LibraryServiceExeption in createLoan on Facade

--- api/src/main/java/cz/muni/fi/pa165/facade/LoanFacade.java
+++ api/src/main/java/cz/muni/fi/pa165/facade/LoanFacade.java
@@ -5,6 +5,7 @@ import java.util.List;
 import cz.muni.fi.pa165.dto.CreateLoanDTO;
 import cz.muni.fi.pa165.dto.LoanDTO;
 import cz.muni.fi.pa165.enums.BookState;
+import cz.muni.fi.pa165.exceptions.LibraryServiceException;
 
 /**
  * Facade layer for Loan entity
@@ -16,9 +17,10 @@ public interface LoanFacade {
      * Persists loan into database
      *
      * @param loan loan object to be created
+     * @throws LibraryServiceException when list of book ids is empty
      * @return new loans id
      */
-    List <Long> createLoan(CreateLoanDTO loan);
+    List <Long> createLoan(CreateLoanDTO loan) throws LibraryServiceException;
 
     /**
      * Returns loan with given id or null
